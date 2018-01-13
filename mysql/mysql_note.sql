e75fd512757a00a434a1f0f19061b0097d55e3e0
tangjian <qsxadf110@126.com>
2018-01-13 17:29:35 +0800

    add mysql notes

--- mysql/mysql_note.sql
+++ mysql/mysql_note.sql
@@ -34,21 +34,29 @@
 -- LONGTEXT		0-4 294 967 295	字节	极大文本数据
 
 CREATE TABLE IF NOT EXISTS 表名(
-	  # 设置主键  auto_increment 属性只用于整数类型，一个表中最多只能有一个auto_increment列
+	# 设置主键  auto_increment 属性只用于整数类型，一个表中最多只能有一个auto_increment列
     id INT UNSIGNED not null AUTO_INCREMENT comment '主键',
 
     # 常用于 类型、记录状态等
     字段名 tinyint 	UNSIGNED NOT NULL default 0 comment '类型',
     # 小于指定的位数将填0，指定为zerofill，则自动添加unsigned属性
     字段名 INT(5) ZEROFILL NOT NULL default 0 comment '类型',
+    # 会删除末尾空格
     字段名 CHAR(10) not null default '' comment '名字',
+    # 忽略大小写，插入不在指定范围的值，会自动选择第一个值插入；一次只能取单个值
     字段名 ENUM('fish', 'apple', 'dog') not null comment '',
+    -- insert into field values('fish');
+    # set 和 enum 非常相似，但set可以一次选取多个值，超出允许范围的值将插入失败，重复成员的值将只取一次
+    字段名 SET('fish', 'apple', 'dog') not null comment '',
+    -- insert into field values('fish, apple');
     字段名 VARCHAR(32) 				not null 						comment '名字',
     # 定点数   适合用来表示货币或高精度的数据
     字段名 decimal(6,2) 			not null default 0  			comment '身高',
     字段名 DATE 					not null default '0000-00-00'  	comment '出厂时间',
     # 获取数字值，应添加 +0      msyql只给第一个timestamp字段设置默认值为系统时间，如果有第二个timestamp，则默认为0值
+    # 插入日期，会先转换为本地时区存放；取出后也会将日期转化为本地时区后显示
     字段名 TIMESTAMP not null default current_timestamp comment '出厂时间',
+    字段名 TIMESTAMP not null default current_timestamp ON UPDATE current_timestamp comment '最后修改的时间',
     字段名 TIMESTAMP not null default '0000-00-00 00:00:00' comment '出厂时间',
     # 默认4位格式 1901~2115和0000     2位格式  70~69，表示1970~2069（5.5.27，已不再支持）
     字段名 YEAR not null default '0000'  	comment '年份',
@@ -66,7 +74,14 @@ AUTO_INCREMENT = 100;
 
 # 查看警告
 show warnings;
-
+# 查看mysql当前时区    SYSTEM => 默认和当前主机的时区一致
+SHOW VARIABLES LIKE 'time_zone';
+# 查看当前数据库支持的表引擎
+SHOW ENGINES \G;
+# 修改时区
+SET TIME_ZONE = '+9:00';
+# 关闭外键约束    0 关闭  1 开启
+SET FOREIGN_KEY_CHECKS = 0;
 /**
  * mysql有初始化的4个数据库
  * infomation_schema      主要存储了系统中的一些数据库对象信息，比如用户表信息、列信息、权限信息、字符集信息、分区信息等
@@ -100,10 +115,14 @@ DESC 表名;
 SHOW CREATE TABLE 表名 \G;
 # 删除表
 DROP TABLE 表名;
-# 查看表状态
+# 查看表状态（对于innnodb - 外键信息）
 SHOW TABLE STATUS LIKE '表名';
 # 清空表
 TRUNCATE TABLE 表名;
+# 优化表（因为text或blog带来的性能问题）
+OPTIMIZE TABLE 表名;
+# 查询当前服务器的字符集和校对规则
+SHOW VARIABLES LIKE 'character_set_server';
 /**
  * 修改表引擎，方法一
  * 会执行很长时间，mysql会按行将数据从原表复制到一张新表，复制期间会消耗所有I/O能力，同时原表会读锁
@@ -141,10 +160,17 @@ ALTER TABLE 表名 DROP COLUMN 字段名;
 ALTER TABLE 表名 CHANGE 字段名 字段名1 int;
 -- change和modify都可以修改表的定义，不同的是change后面需要写两次列名，不方便。但是change的优点是可以修改列名称，modify则不能；
 
+
+/**
+ * INSERT 语句
+ */
 # 插入记录，多条
 INSERT INTO 表名 ( field1, field2, ..., fieldN ) VALUES ( value1, value2, ..., valueN ), ( value1, value2, ..., valueN );
 # 插入记录，value顺序应该和字段的排列顺序一致
 INSERT INTO 表名 VALUES (value1, value2, ..., valueN);
+# 插入散列值
+INSERT INTO 表名 VALUES (repeat('beijing',2), md5(字段名));
+
 
 # 更新字段值
 UPDATE 表名 SET field1=value1, field2=value2 [WHERE CONDITION];
@@ -168,6 +194,22 @@ SELECT * FROM 表1
 UNION
 SELECT * FROM 表2;
 
+
+/**
+ * 创建索引
+ *
+ * 最适合索引的列是出现在where子句中的列，或连接子句中的列；有区分度的索引
+ * 使用短索引
+ * 利用最左前缀（多列索引可起几个索引的作用，因为可以利用索引中最左边的列集来匹配行）
+ * 索引，会降低写操作的性能
+ * InnoDB表的普通索引都会保存主键的键值，所以主键要尽可能的短
+ */
+
+# 为表创建10个字节的前缀索引
+CREATE INDEX 索引名 ON 表名 (字段名(10));
+# 删除索引
+DROP INDEX 索引名 ON 表名;
+
 /**
  * 聚合
  * having 对聚合后的结果进行条件过滤
@@ -181,10 +223,225 @@ SELECT concat('drop table 库名.', table_name, ';') FROM tables WHERE table_sch
 SELECT concat('alter table 库名.', table_name, ' engine=innodb;') FROM tables WHERE table_schema='库名' and engine = 'MyISAM';
 
 
+/**
+ * 事件调度
+ */
+
+# 每隔5秒 执行一次sql
+CREATE EVENT 事件名
+    ON SCHEDULE
+        EVERY 5 SECOND
+    DO
+        UPDATE 数据库.表名 SET 字段=字段+1;
+# 查看调度器状态
+SHOW EVENTS \G;
+# 查看事件调度器状态
+SHOW VARIABLES LIKE '%scheduler%';
+# 打开调度器
+SET GLOBAL EVENT_SCHEDULER = 1;
+# 查看mysql进程
+SHOW PROCESSLIST \G;
+# 禁用事件调度器
+ALTER EVENT 事件名 DISABLE;
+# 删除
+DROP EVENT 事件名;
+
+/**
+ * 触发器
+ *
+ * 只能创建在永久表上，不能返回数据，不可开启事务；对插入、更细、删除会有性能影响
+ * before after
+ * insert update
+ */
+
+# 创建触发器
+CREATE TRIGGER 触发器名
+    AFTER INSERT ON 表名 FOR EACH ROW BEGIN
+    INSERT INTO 表名1 VALUES (val1, val2);
+END;
+# 删除触发器
+DROP TRIGGER 触发器名;
+# 查看触发器
+SHOW TRIGGERS \G;
+
+# 此模式，可将反斜线变为普通字符
+no_backslash_escapes
+
+
+/**
+ * 表分区
+ *
+ * 线性hash的优点：分区维护更加方便；缺点：各个分区之间的数据分布不太均匀
+ */
+CREATE TABLE 表名(
+    ...
+)
+-- key 分区
+PARTITION by KEY (字段) PARTITIONS 4;
+
+# 删除分区，也同时删除了该分区的数据
+ALTER TABLE 表名 DROP PARTITION 分区名;
+# 为range或list分区表添加一个分区
+ALTER TABLE 表名 ADD PARTITION (PARTITION 分区名 VALUES LESS THAN (expr));
+
+
+/**
+ * 优化
+ */
+
+# 查询每条语句执行的次数   SHOW [session|global] STATUS 默认session
+SHOW STATUS LIKE 'Com_%';
+# 查询语句执行情况
+EXPLAIN SELECT * FROM 表名 WHERE 字段名 = 'xxx' \G;
+-- 相关选项说明
+-- type     all, index, range, ref, eq_ref, const/system, null      从左至右，性能由最差到最好
+-- possible_keys    查询时可能用到的索引
+-- key              查询时实际用到的索引
+-- key_len          使用到的索引字段长度
+-- rows             扫描行的数量
+-- extra            执行情况的描述和说明
+
+# 优化表
+-- 适用于删除了表的一大部分，或者对含有可变长度的行的表（varchar，blog，text）进行了很多更改
+-- 可对表的空间碎片进行合并，避免空间浪费
+-- 只对myisam、bdb、innodb表起作用
+-- 会对表进行锁定
+optimize TABLE 表名;
+
+/**
+ * 大批量插入数据
+ */
+
+# 打开或关闭myisam表非唯一索引的更新，在导入数据时，设置这两个命令，可提高导入效率
+-- 使用于导入大量数据到一个非空的myisam表，不适用于innodb表
+ALTER TABLE 表名 DISABLE KEYS;
+LOAD DATA INFILE '/home/mysql/table_data.txt' INTO TABLE 表名;
+ALTER TABLE 表名 ENABLE KEYS;
+# innodb表，提高导入效率
+-- 将导入的顺序按照主键的顺序排列
+-- 导入数据前关闭唯一性校验
+SET UNIQUE_CHECKS = 0;
+-- 导入结束后，打开唯一性校验
+SET UNIQUE_CHECKS = 1;
+
+
+/**
+ * show profile 分析sql
+ */
+
+# 是否支持profile
+SELECT @@have_profiling;
+# 是否打开profile
+SELECT @@profiling;
+# 打开profile（在session级别开启）
+SET PROFILING = 1;
+# 可以看到执行的sql语句列表，其中有query_id
+SHOW PROFILES;
+# 可以看到指定query_id的sql语句在执行过程中线程的每个状态和消耗的时间
+SHOW PROFILE FOR QUERY 4;
+
+# 查看索引的使用情况
+SHOW STATUS LIKE 'Handler_read%';
+-- 如果索引正在工作 handler_read_key 的值会很高
+-- 如果 handler_read_rnd_next 的值高则意味着查询效率低
+
+
+/**
+ * 查询缓存
+ */
+SHOW VARIABLES LIKE '%query_cache%';
+-- have_query_cache     是否配置了高速缓存
+-- query_cache_size     缓存区大小
+-- query_cache_type     0-关闭 1-打开 2-只有带sql_cache提供高速缓存
+
+
+/**
+ * mysqldump备份数据库
+ */
+-- -d   --no-data           不包含数据
+-- -n   --no-create-db      不包含数据库的创建语句
+-- -t   --no-create-info    不包含表的创建语句
+--      --compact           不包含默认选项中的各种注释
+-- -c   --complete-insert   导出的insert语句中包含字段名称
+-- -F   --flush-logs        备份前关闭旧日志，生成新日志，大大方便恢复过程
+-- -l   --lock-tables       给所有表加读锁，备份期间数据无法被更新，可以配合-F选项一起使用
+-- $ mysqldump -h127.0.0.1 -p3306 -uroot -pqsxadf110 数据库名 > test.txt
+-- $ mysqldump -h127.0.0.1 -p3306 -uroot -pqsxadf110 -d 数据库名 > test.txt
+
+# 备份
+-- 会在指定目录生成两个文件，一个是.sql（建表语句），另一个是.txt（存放实际数据）
+-- $ mysqldump -uroot -pqsxadf110 -d 数据库名 表名 -T 路径
+# 手工设置字符集
+-- $ mysqldump -uroot -pqsxadf110 --default-character-set=latin1 数据库名 表名 -T 路径
+
+/**
+ * mysqlimport  数据导入工具
+ */
+---------------------------------------------------
+
+now()                       当前日期
+from_unixtime               时间戳转换为日期
+unix_timestamp              日期转换为时间戳
+LAST_INSERT_ID              当前线程最近一次的自增值，针对所有表
+length                      字符串长度
+mod(3,2)                    模运算，求余，与3%2效果一样
+
+concat(s1, s2,...sn)        连接s1,s2,...sn为一个字符串
+insert(str, x, y, instr)    将字符串str中第x个位置开始，y个字符串长的字符串替换为instr
+lower(str)                  转小写
+upper(str)                  转大写
+left(str,x)                 返回字符串str最左边的x个字符
+right(str,x)                返回字符串str最右边的x个字符
+lpad(str,n,pad)             用字符串pad对str最左边进行填充，直到长度为n个字符长度
+rpad(str,n,pad)             用字符串pad对str最右边进行填充，直到长度为n个字符长度
+ltrim(str)                  去除str左侧空格
+rtrim(str)                  去除str右侧空格
+trim(str)                   去除str两侧空格
+repeat(str,x)               返回str重复x次的结果
+replace(str,a,b)            用字符串b替换str中所有出现的字符串a
+substring(str,x,y)          返回str从x起y个长度的字符串
+strcmp(s1, s2)              比较s1和s2的ascii的码值的大小     小：-1 相等：0 大：1
+
+ceil(x)                     大于x的最小整数
+floor(x)                    小于x的最大整数
+rand()                      返回0~1内的随机值  eg: ceil(100*rand()) 0~100内的随机整数
+round(x,y)                  返回参数x的四舍五入的有y位小数的值，y默认为0
+turncate(x,y)               返回数字x截断为y位小数的结果，与round区别：仅仅是截断，不进行四舍五入
+
+curdate                     当前日期    2007-07-11
+curtime                     当前时间    14:13:43
+now                         当前日期和时间     2007-07-11 14:13:43
+unix_timestamp(date)        返回日期date的unix时间戳    1184134516
+from_unixtime(timestamp)    返回unixtime时间戳的日期值   2007-07-11 14:13:43
+week(now())                 所给日期是一年中第几周
+year(now())                 所给日期是那一年
+monthname(date)             返回英文月份名称
+date_format(data,fmt)       按照指定格式显示时间  data_format(now(), '%Y-%m-%d')
+datediff(date1, date2)      两个日期相差多少天   datediff('2008-05-05', now())
+
+database()                  返回当前数据库名
+version()                   当前版本
+user()                      当前登录的用户名
+inet_aton(ip)               返回ip的数字表示
+inet_ntoa(num)              返回数字表示的ip
+md5()
+password(str)               返回str的加密版本，一个41位长的字符串
+
 
 ---------------------------------------------------
 
-now()               当前日期
-from_unixtime       时间戳转换为日期
-unix_timestamp      日期转换为时间戳
-LAST_INSERT_ID()    最近一次的自增值，针对所有表
+<=> 和 = 功能相似，在操作数相等时为1，即使操作数的值为null也可以比较
+a between min and max       当a大于等于min并且小于等于max，返回值为1，否则返回0
+is null
+is not null
+regexp       str regexp str_pat   当str字符串中含有str_pat相匹配的字符串时，则返回1，否则返回0
+
+
+if(value,x,y)               如果value为真，返回t否则返回y
+ifnull(val1, val2)          如果val1不为空，返回val1，否则返回val2
+CASE WHEN val1 THEN [res1] ELSE [default] END               如果val1为真，返回res1，否则default
+CASE [expr] WHEN val1 THEN [res1] ELSE [default] END        如果expr等于val1，返回res1，否则default
+
+
+
+msyql
